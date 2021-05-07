719fc70e9c0ece9a3ea8e3385334a9a2cf98c3a8
rever1300 <rever1300@gmail.com>
2021-05-07 16:00:55 +0200

    Changed architecture of server package

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -10,33 +10,59 @@ import java.util.Scanner;
 public class Server {
     private static Registry startRegistry(Integer port)
             throws RemoteException {
-        if(port == null) {
+        if (port == null) {
             port = 1099;
         }
         try {
             Registry registry = LocateRegistry.getRegistry(port);
-            registry.list( );
+            registry.list();
             // The above call will throw an exception
             // if the registry does not already exist
             return registry;
-        }
-        catch (RemoteException ex) {
+        } catch (RemoteException ex) {
             // No valid registry at that port.
             System.out.println("RMI registry cannot be located ");
-            Registry registry= LocateRegistry.createRegistry(port);
+            Registry registry = LocateRegistry.createRegistry(port);
             System.out.println("RMI registry created at port ");
             return registry;
+        }
+    }
+
+    private static boolean start = false;
+
+    private static class Interrupt extends Thread {
+        String interrupt_key = null;
+        Object semaphore = null;
+
+        //semaphore must be the syncronized object
+        public Interrupt(Object semaphore, String interrupt_key) {
+            this.semaphore = semaphore;
+            this.interrupt_key = interrupt_key;
+        }
+
+        public void run() {
+            while (true) {
+                //read the key
+                Scanner scanner = new Scanner(System.in);
+                String x = scanner.nextLine();
+                System.out.println(x);
+                if (x.equals(this.interrupt_key)) {
+                    //if is the key we expect, change the variable, notify and return(finish thread)
+                    synchronized (this.semaphore) {
+                        start = true;
+                        this.semaphore.notify();
+                        return;
+                    }
+                }
+            }
         }
     }
 
     public static void main(String args[]) {
         Scanner pathInput = new Scanner(System.in);
         String startWord = "start";
-        Object semaphore = null;
-        String key_interrupt = null;
-        Server.Interrupted.Interrupt interrupt = new Server.Interrupted.Interrupt(semaphore,key_interrupt);
-
-
+        Object obj = new Object();
+        Interrupt interrupt = new Interrupt(obj, startWord);
 
         try {
             Registry registry = startRegistry(null);
@@ -49,10 +75,9 @@ public class Server {
             System.out.print("Beginning registering...");
 
 
-
-
-        }catch (Exception e){
-            System.err.println("Server exception" + e.toString()); e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Server exception" + e.toString());
+            e.printStackTrace();
         }
     }
 }
